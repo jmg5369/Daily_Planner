18f8e2ea0aace9cce8e45461854565dba4492ea5
James Gorodesky <jmg5369@gmail.com>
2022-03-27 13:40:17 -0600

    finished up color functionality

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var textAreas = document.querySelectorAll("textarea");
 var currentDay = document.getElementById("currentDay");
 
 var timer;
-var currentHour = moment().format("hh")
+var currentHour = moment().format("H")
+
+
 
 setInterval(function() {
     timer = moment().format("MMMM Do, YYYY hh:mm:ss A");
@@ -21,13 +23,24 @@ if(!userStoreageData) {
 
 
 
+
+
 var textAreaIDs = ["9-am", "10-am", "11-am", "12-pm", "13-pm", "14-pm", "15-pm", "16-pm", "17-pm", "18-pm"]
 console.log(textAreas);
 
 for (let i = 0; i < textAreas.length; i++) {
     textAreas[i].value =realUserData[i]
-}
 
+   var specificID = textAreas[i].id.split("-")[0];
+
+   if ( parseInt(specificID) === parseInt(currentHour)) {
+       textAreas[i].classList.add("present")
+   } else if ( parseInt(specificID) < parseInt(currentHour)) {
+    textAreas[i].classList.add("past")
+} else  {
+    textAreas[i].classList.add("future")
+}
+}
 
 for (let i = 0; i < saveButtons.length; i++) {
     saveButtons[i].addEventListener("click", function(){
